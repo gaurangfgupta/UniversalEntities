1b4299961f783ac88053a6b998d2614a0d5028d2
Gaurang Gupta <gaurangfgupta@gmail.com>
2015-12-22 13:57:17 +0530

    Removed structs from all Interfaces and created few new interfaces

--- UniversalEntities/UniversalEntities/Interfaces/IMobile.cs
+++ UniversalEntities/UniversalEntities/Interfaces/IMobile.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace UniversalEntities.Interfaces
 {
-    interface IPhone
+    interface IMobile
     {
         string CountryCode { get; set; }
-        string StdCode { get; set; }
         string Number { get; set; }
     }
 }

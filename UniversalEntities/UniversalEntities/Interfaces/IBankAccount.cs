37813d653e259fb03f42ceb6b6dac797e9c7f9e6
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-01-12 16:30:59 +0530

    Added Bank account interface

---
+++ UniversalEntities/UniversalEntities/Interfaces/IBankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalEntities.Interfaces
+{
+    interface IBankAccount
+    {
+        /// <summary>
+        /// The name of the bank
+        /// </summary>
+        string BankName { get; set; }
+        /// <summary>
+        /// The bank account number (not the Customer ID)
+        /// </summary>
+        string AccountNumber { get; set; }
+        /// <summary>
+        /// Minimum 11 characters (info to implement validation)
+        /// </summary>
+        string IFSC { get; set; }
+        string MICR { get; set; }
+        string Branch { get; set; }
+    }
+}

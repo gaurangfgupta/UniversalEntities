b4f2368a95413a8d9ff6f96576ac621784cf5342
Shivani Gupta <helloshivani21apr@gmail.com>
2016-01-14 17:03:12 +0530

    Created abstract classes
    Added few abstract classes

---
+++ UniversalEntities/UniversalEntities/Abstracts/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalEntities.Abstracts
+{
+    abstract class Contact
+    {
+        public PersonName Name { get; set; }
+        public List<Address> Addresses { get; set; }
+        public List<Phone> Phones { get; set; }
+        public List<Mobile> Mobiles  { get; set; }
+        public List<Email> Emails { get; set; }
+
+    }
+
+    
+}
---
+++ UniversalEntities/UniversalEntities/Abstracts/DetailedPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalEntities.Abstracts
+{
+    class DetailedPerson:Contact
+    {
+        public PersonName HusbandFatherName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public byte Age { get; set; }
+        public string Sex { get; set; }
+        public string Religion { get; set; }
+        public string MaritalStatus { get; set; }
+        public DateTime DateOfRegistration { get; set; }
+        
+    }
+}

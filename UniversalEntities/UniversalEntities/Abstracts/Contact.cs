e34ed67205b1bf309f60c113fd5c1d1f8d49d8e1
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-01-17 12:52:58 +0530

    Restructured
    Few Interfaces now have List of strings unlike earlier. The structs have
    been removed as they are no more required.
    The Contact abstract class directly implements the interafaces

--- UniversalEntities/UniversalEntities/Abstracts/Contact.cs
+++ UniversalEntities/UniversalEntities/Abstracts/Contact.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace UniversalEntities
 {
-    public abstract class Contact
+    public abstract class Contact : IMobiles, IPhones, IEmails
     {
         public PersonName Name { get; set; }
         public List<Address> Addresses { get; set; }
-        public List<Phone> Phones { get; set; }
-        public List<Mobile> Mobiles  { get; set; }
-        public List<Email> Emails { get; set; }
-
+        public List<string> Phones { get; set; }
+        public List<string> Mobiles { get; set; }
+        public List<string> Emails { get; set; }
     }
 }

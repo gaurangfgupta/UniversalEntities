8b8c77071c1c235862d071a216c97fd75a7c26e3
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-02-29 14:08:47 +0530

    Address now returns its string form

--- UniversalEntities/UniversalEntities/Abstracts/IAddress.cs
+++ UniversalEntities/UniversalEntities/Abstracts/IAddress.cs
@@ -24,18 +24,54 @@ namespace UniversalEntities
     }
     public class Address : IAddress
     {
-        public string City { get; set; }
-
-        public string Country { get; set; }
-
         public string Line1 { get; set; }
 
         public string Line2 { get; set; }
 
         public string Line3 { get; set; }
 
+        public string City { get; set; }
+
         public string PinCode { get; set; }
 
         public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder addressBuilder = new StringBuilder();
+            if (!string.IsNullOrWhiteSpace(Line1))
+            {
+                addressBuilder.AppendLine(Line1);
+            }
+            if (!string.IsNullOrWhiteSpace(Line2))
+            {
+                addressBuilder.AppendLine(Line2);
+            }
+            if (!string.IsNullOrWhiteSpace(Line3))
+            {
+                addressBuilder.AppendLine(Line3);
+            }
+            if (!string.IsNullOrWhiteSpace(City))
+            {
+                addressBuilder.AppendLine(City);
+            }
+            if (!string.IsNullOrWhiteSpace(PinCode))
+            {
+                addressBuilder.AppendLine(PinCode);
+            }
+            if (!string.IsNullOrWhiteSpace(State))
+            {
+                addressBuilder.AppendLine(State);
+            }
+            if (!string.IsNullOrWhiteSpace(Country))
+            {
+                addressBuilder.AppendLine(Country);
+            }
+            string address = addressBuilder.ToString().Trim();
+            addressBuilder = null;
+            return address;
+        }
     }
 }

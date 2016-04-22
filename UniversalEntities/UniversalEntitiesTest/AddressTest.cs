cc63bd657dc8a37a6f0227feb25ec41ed705fa33
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-04-22 21:21:47 +0530

    Begun coding Address sorting
    Unit testing in progress

---
+++ UniversalEntities/UniversalEntitiesTest/AddressTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniversalEntities;
+
+namespace UniversalEntitiesTest
+{
+    [TestClass]
+    public class AddressTest
+    {
+        [TestMethod]
+        public void SortAddressLines()
+        {
+            Address a1 = new Address();
+            a1.Line1 = "1";
+            a1.Line2 = "";
+            a1.Line3 = "1";
+            a1.SortAddressLines();
+            Assert.AreEqual("1", a1.Line1);
+            Assert.AreEqual("1", a1.Line2);
+            Assert.AreEqual("", a1.Line3);
+        }
+    }
+}
